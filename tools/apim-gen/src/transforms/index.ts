b635f16844ae99ff5b6099315c7d3aca09bf8d26
Bruno Amui <brunoamui@gmail.com>
2026-02-06 16:06:10 -0300

    Add apim-gen OpenAPI transforms
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-Claude)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ tools/apim-gen/src/transforms/index.ts
@@ -0,0 +1,8 @@
+export { splitByPrefix } from "./split-by-prefix.js";
+export { removeParams } from "./remove-params.js";
+export { removeFalsePathParams } from "./remove-false-path-params.js";
+export { removePaths } from "./remove-paths.js";
+export { setOpenapiVersion } from "./set-openapi-version.js";
+export { ensureOperationId } from "./ensure-operation-id.js";
+export { addDescriptionNote } from "./add-description-note.js";
+export { cleanupComponents } from "./cleanup-components.js";
